62918d7714c771202bbfbf64c85725395f8ceebd
Will <will.soto9@gmail.com>
2015-02-27 08:21:29 -0500

    Add build.sh instead of using Make

---
+++ build.sh
@@ -0,0 +1,20 @@
+FILES=./sublime/*.tmTheme
+
+rm -rf "./atom"
+mkdir "./atom";
+
+for file in $FILES; do
+    echo "Processing $file ..";
+
+    filename=$(basename "$file")
+    filename="${file%.*}"
+    filename="${filename##*/}"
+
+    directory="./atom/$filename"
+
+    apm init --theme $directory --convert $file
+
+    rm "$directory/.gitignore" "$directory/CHANGELOG.md" "$directory/README.md"
+
+    echo "Converted $file"
+done
